45587b2d97f4fdf35cbc15e4589d5efd2e061746
leonidasleon50-stack <leonidasleon50@gmail.com>
2026-08-14 13:56:47 -0300

    aula 3

--- AE/Aula11/Aula11.py
+++ AE/Aula11/Aula11.py
@@ -6,11 +6,26 @@ import matplotlib.pyplot as plt
 u = 2.71828182846
 
 # critério de parada 
-n = 6 
-Eppara = 0.5*10**(2-n)
+na = 6 
+Eppara = 0.5*10**(2-na)
 
 print(Eppara)
 
 def serieMaclarium(x,n):
     return (x**n)/math.factorial(n)
+
+#definição de x
+x = 1 
+
+#inicialização de variáveis
+
+soma  = 0 
+#Número de termos 
+n = 6
+for i in range(0,n-1): #vou varia para toda essa faixa 
+
+    soma += serieMaclarium(x,i)
+    pritn("A soma e:",soma)
+
+
 
